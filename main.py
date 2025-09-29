fa2c18e5f76bb818052949e368f3d7e37ad4960f
Davi Tomé <Daviptome2008@gmail.com>
2025-09-29 14:42:09 -0300

    Criação do Banco e Tabela

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import sqlite3
+
+#conectando
+conexao = sqlite3.connect("biblioteca.db")
+cursor = conexao.cursor()
+
+#Criando a Tabela
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS  livros (
+    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,        
+    autor TEXT NOT NULL,  
+    ano INTEGER,    
+    disponivel TEXT     
+    )
+""")
+conexao.commit()
+conexao.close()
